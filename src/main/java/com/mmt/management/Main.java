a7a4282fdc0502755bb7b33dd40702758abf44ab
HuZhaoLiang <hp393747035@126.com>
2019-07-02 23:27:23 +0800

    git commit

--- src/main/java/com/mmt/management/Main.java
+++ src/main/java/com/mmt/management/Main.java
@@ -2,6 +2,7 @@ package com.mmt.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Main 
@@ -10,6 +11,11 @@ public class Main
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(Main.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure( SpringApplicationBuilder builder) 
+	{
+        return builder.sources(this.getClass());
 	}
 
 }
